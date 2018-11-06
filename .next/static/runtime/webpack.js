d6afe15cfd49f7ae3b8efdc12bba340f9200381d
shadid2018 <shadidhaque2012@gmail.com>
2018-11-06 17:38:14 -0500

    only numerics to go

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ced71d53a1739cee9cfc";
+/******/ 	var hotCurrentHash = "0e02c5aeba531c6564c4";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/Forms/Consumer.js
+++ components/Forms/Consumer.js
@@ -4,6 +4,11 @@ import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Switch from '@material-ui/core/Switch';
+import Checkbox from '@material-ui/core/Checkbox';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
 
 import { connect } from 'react-redux';
 
@@ -72,6 +77,94 @@ class Consumer extends React.Component {
                                             />
                                         </div>
                                     )
+                                case 'multi-choice':
+                                    return(
+                                        <div>
+                                            <div>{item.label}</div>
+                                            {
+                                                item.options.map((option, index) => {
+                                                    return(
+                                                        <div>
+                                                        
+                                                        <Checkbox 
+                                                            checked={option.value} 
+                                                            onChange={() => {
+
+                                                                let itemsCopy = this.state.items;
+                                                                // fidn index
+                                                                this.state.items.map((aItem, i) => {
+                                                                    if(aItem.uid == item.uid) {
+                                                                        itemsCopy[i].options[index].value 
+                                                                            = !itemsCopy[i].options[index].value
+
+                                                                        const {dispatch} = this.props
+                                                                        dispatch(updateItems(itemsCopy))
+                                                                        this.setState({
+                                                                            items: itemsCopy
+                                                                        })
+                                                                    }
+                                                                })
+                                                            }}
+                                                            value={option.value} 
+                                                        />
+                                                        <span>{option.label}</span>
+                                                        </div>
+                                                    )
+                                                })
+                                            }
+                                        </div>
+                                    )
+
+                                case 'calander':
+                                    return(
+                                        <div>
+                                            <div>{item.q_label}</div>
+                                            <TextField
+                                                type="datetime-local"
+                                                className={classes.textField}
+                                                defaultValue="2017-05-24T10:30"
+                                                // onChange={this.handleTextChange}
+                                                margin="normal"
+                                            />
+                                        </div>
+                                    )
+                                case 'dropdown':
+                                    return(
+                                        <div>
+                                            <FormControl className={classes.formControl}>
+                                                <InputLabel htmlFor="age-simple">{item.q_label}</InputLabel>
+                                                <Select
+                                                    value={item.selected}
+                                                    onChange={(event) => {
+                                                        let itemsCopy = this.props.items;
+                                                        // fidn index
+                                                        this.props.items.map((aItem, i) => {
+                                                            if(aItem.uid == item.uid) {
+                                                                itemsCopy[i].selected
+                                                                    =  event.target.value
+
+                                                                const {dispatch} = this.props
+                                                                dispatch(updateItems(itemsCopy))
+                                                                this.setState({
+                                                                    model: itemsCopy
+                                                                })
+                                                            }
+                                                        })
+                                                    }}
+                                                    inputProps={{
+                                                        name: 'age',
+                                                        id: 'age-simple',
+                                                    }}
+                                                >
+                                                    {
+                                                        item.options.map((o, index) => {
+                                                            return (<MenuItem value={index}>{o.label}</MenuItem>)
+                                                        })
+                                                    }
+                                                </Select>
+                                            </FormControl>
+                                        </div>
+                                    )
                                 default:
                                     return(
                                         <div>Hello man</div>
@@ -101,6 +194,10 @@ const styles = theme => ({
     },
     root: {
         padding: '30px'
+    },
+    formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 270,
     }
 });
 
